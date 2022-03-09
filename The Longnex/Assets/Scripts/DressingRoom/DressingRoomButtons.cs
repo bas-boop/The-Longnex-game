926e3e6597ef17c069985a1c653d9ced2770512a
Bas <30810@ma-web.nl>
2022-03-09 10:44:18 +0100

    Index kan veranderd worden met de knopjes

--- The Longnex/Assets/Scripts/DressingRoom/DressingRoomButtons.cs
+++ The Longnex/Assets/Scripts/DressingRoom/DressingRoomButtons.cs
@@ -1,26 +1,28 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
+//using System;
+//using System.Collections;
+//using System.Linq;
+using System.Collections.Generic;
 
 public class DressingRoomButtons : MonoBehaviour
 {
     //[SerializeField]private GameObject[] head;
     [SerializeField]private List<GameObject> head = new List<GameObject>();
 
-    public void NextItem()
+    private int _index = 0;
+
+    public void NextItem()//Omhoog knop
     {
-        for (int i = 0; i < head.Count; i++)
-        {
-            Debug.Log(head[i]);
-        }
+        _index = _index + 1;
+        if (_index == head.Count)
+            _index = _index = 0;
+        Debug.Log(head[_index]);
     }
-    public void PreviousItem()
+    public void PreviousItem()//Omlaag knop
     {
-        for (int i = 0; i < head.Count; i--)
-        {
-            Debug.Log(head[i]);
-        }
+        _index = _index - 1;
+        if (_index < 0)
+            _index = head.Count - 1;
+        Debug.Log(head[_index]);
     }
 }
