3839b50835fd71d9c196fa11ad50161a0a257854
Bas <30810@ma-web.nl>
2022-03-09 15:00:00 +0100

    de items gaan actief waneer het mag van de knoppen

--- The Longnex/Assets/Scripts/DressingRoom/DressingRoomButtons.cs
+++ The Longnex/Assets/Scripts/DressingRoom/DressingRoomButtons.cs
@@ -1,28 +1,46 @@
+using System;
 using UnityEngine;
 //using System;
 //using System.Collections;
 //using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 
 public class DressingRoomButtons : MonoBehaviour
 {
-    //[SerializeField]private GameObject[] head;
     [SerializeField]private List<GameObject> head = new List<GameObject>();
 
     private int _index = 0;
+    [SerializeField] private GameObject currentItem;
 
+    private void Start()
+    {
+        currentItem = head[0];
+        currentItem = head[_index];
+    }
+    
     public void NextItem()//Omhoog knop
     {
+        currentItem.SetActive(false);
         _index = _index + 1;
         if (_index == head.Count)
             _index = _index = 0;
+
+        currentItem = head[_index];
+        currentItem.SetActive(true);
+        
         Debug.Log(head[_index]);
     }
     public void PreviousItem()//Omlaag knop
     {
+        currentItem.SetActive(false);
         _index = _index - 1;
         if (_index < 0)
             _index = head.Count - 1;
+        
+        currentItem = head[_index];
+        currentItem.SetActive(true);
+        
         Debug.Log(head[_index]);
     }
 }
