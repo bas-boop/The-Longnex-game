b2fb53af725d246a005d418dfa169a95adc0359b
Bas <30810@ma-web.nl>
2022-03-22 13:29:17 +0100

    locked items hebben nu een lock icon er over heen

--- The Longnex/Assets/Scripts/DressingRoom/HeadDressingRoomButtons.cs
+++ The Longnex/Assets/Scripts/DressingRoom/HeadDressingRoomButtons.cs
@@ -1,8 +1,8 @@
 using System;
 using UnityEngine;
-//using System;
-//using System.Collections;
-//using System.Linq;
+using System;
+using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 
 public class HeadDressingRoomButtons : MonoBehaviour
@@ -10,28 +10,30 @@ public class HeadDressingRoomButtons : MonoBehaviour
     [SerializeField] private List<GameObject> head = new List<GameObject>();
     private int _index = 0;
     [SerializeField] private GameObject currentItem;
-    //[SerializeField] private GameObject nextItem;
+    [SerializeField] private GameObject nextItem;
     //[SerializeField] private GameObject previousItem;
+
+    [SerializeField]private GameObject _lock;
 
     private void Start()
     {
+        _index = 0;
         currentItem = head[_index];
-        //nextItem = head[_index + 1];
-        //previousItem = head[_index - 1];
+
+        _lock = GameObject.Find("LockH");
+        _lock.SetActive(false);
     }
     
     public void NextItem()
     {
         currentItem.SetActive(false);
         _index += 1;
-        if (currentItem.CompareTag("lockedItem"))
+        if (head[_index].CompareTag("lockedItem"))
         {
-            if (_index == head.Count)
-                _index = 0 - 1;
-            currentItem.SetActive(false);
-            _index += 1;
-            currentItem = head[_index];
-        }
+            _lock.SetActive(true);
+        }else
+            _lock.SetActive(false);
+            
         if (_index == head.Count)
             _index = 0;
 
@@ -39,17 +41,18 @@ public class HeadDressingRoomButtons : MonoBehaviour
         currentItem.SetActive(true);
         
         Debug.Log(head[_index]);
+        Debug.Log(_index);
     }
     public void PreviousItem()
     {
         currentItem.SetActive(false);
         _index = _index - 1;
-        if (currentItem.CompareTag("lockedItem"))
+        if (head[_index].CompareTag("lockedItem"))
         {
-            currentItem.SetActive(false);
-            _index -= 1;
-            currentItem = head[_index];
-        }
+            _lock.SetActive(true);
+        }else
+            _lock.SetActive(false);
+
         if (_index < 0)
             _index = head.Count - 1;
         
