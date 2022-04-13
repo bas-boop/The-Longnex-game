2ad9eb1cc8eceef0075ac5efd8f5e3d6b3659d49
Bas <30810@ma-web.nl>
2022-04-13 13:05:19 +0200

    start van pong

---
+++ The Longnex/Assets/Scripts/Pong/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+
+    private void Update()
+    {
+        /*if (Input.GetAxisRaw("Horizontal1"))
+        {
+            
+        }*/
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+        }
+    }
+}
