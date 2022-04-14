54ace224b22f9f9d1d2ae08a76818bdeb35d736f
Bas <30810@ma-web.nl>
2022-04-14 11:02:48 +0200

    player movement is er

--- The Longnex/Assets/Scripts/Pong/PlayerMovement.cs
+++ The Longnex/Assets/Scripts/Pong/PlayerMovement.cs
@@ -8,16 +8,33 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private GameObject player1;
     [SerializeField] private GameObject player2;
 
+    [SerializeField] private float playerSpeed;
+
+    private Rigidbody2D _rdp1;
+    private Rigidbody2D _rdp2;
+
+    private void Start()
+    {
+        _rdp1 = player1.GetComponent<Rigidbody2D>();
+        _rdp2 = player2.GetComponent<Rigidbody2D>();
+    }
+
     private void Update()
     {
-        /*if (Input.GetAxisRaw("Horizontal1"))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            
-        }*/
-
-        if (Input.GetKey(KeyCode.A))
+            player1.transform.position += Vector3.up * playerSpeed;
+        }else if (Input.GetKey(KeyCode.DownArrow))
         {
-            
+            player1.transform.position -= Vector3.up * playerSpeed;
+        }
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            player2.transform.position += Vector3.up * playerSpeed;
+        }else if (Input.GetKey(KeyCode.S))
+        {
+            player2.transform.position -= Vector3.up * playerSpeed;
         }
     }
 }
